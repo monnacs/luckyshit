5bece66c3eae6e45c6212460148676e74a97eca5
Caue Rego <caue@cregox.com>
2014-09-13 02:06:13 -0300

    make scenario go left and back
    also add startup variables

--- Assets/MoveChar.js
+++ Assets/MoveChar.js
@@ -1,16 +1,18 @@
 ﻿#pragma strict
 
+var moveHowMuch = 0.1;
+
 function Start () {
 
 }
 
 function Update () {
 	if (Input.GetKey(KeyCode.UpArrow))
-		transform.position.y += 0.1;
+		transform.position.y += moveHowMuch;
 	if (Input.GetKey(KeyCode.DownArrow))
-		transform.position.y -= 0.1;
+		transform.position.y -= moveHowMuch;
 	if (Input.GetKey(KeyCode.RightArrow))
-		transform.position.x += 0.1;
+		transform.position.x += moveHowMuch;
 	if (Input.GetKey(KeyCode.LeftArrow))
-		transform.position.x -= 0.1;
+		transform.position.x -= moveHowMuch;
 }
