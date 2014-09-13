28ffd55003ccad34a3dc6ae43c9ba8c7b889692d
Monna Cleide <monnacs@gmail.com>
2014-09-13 18:32:05 -0300

    Updating friends

---
+++ Assets/Scoreboard.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var playerLives: int;
+static var playerScore: int;
+
+function Start () {
+
+}
+
+function Update () {
+	var score: String = "";
+	score = playerScore.ToString();
+	GetComponent(TextMesh).text = score;
+}
