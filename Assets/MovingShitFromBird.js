e64b3340bdc3512d473a3817da3b6c96b5f6f016
brasileirorodrigo <brasileiro.rodrigo1@gmail.com>
2014-09-13 14:18:28 -0300

    shot with space
    Pessoa transparente

---
+++ Assets/MovingShitFromBird.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+
+var moveHowMuch = 0.1;
+var floorObj:GameObject;
+
+
+private var initialPosition;
+
+function Start () {
+	
+	initialPosition = transform.position;
+}
+
+function Update () {
+	transform.position.y -= moveHowMuch;
+}
+
+function OnCollisionEnter (col:Collision){
+Debug.Log(col);  
+if(col.gameObject == floorObj)
+ {
+     Destroy(col.gameObject);
+    }
+}
---
+++ Assets/ShitLaunch.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var shitPrefab:GameObject;
+
+function Start () {
+	
+		shitPrefab.transform.position = transform.position ;
+	
+}
+
+function Update () {
+	if(Input.GetKeyDown("space")){
+
+		Instantiate(shitPrefab, transform.position, transform.rotation);
+		
+
+     }
+}
