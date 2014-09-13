a9b24df4b187d472a346cbb1a2558e2c7f5ced63
Caue Rego <caue@cregox.com>
2014-09-13 14:08:56 -0300

    failed attempt to randomize scenario
    also prefabing and organizing stuff
    and add camera for GUI

---
+++ Assets/scripts/MoveLeft.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+#pragma downcast
+
+var moveHowMuch = 0.1;
+var repeatAfter = 20.0;
+private var initialRepeatAfter : float;
+private var initialPosition : Vector3;
+private var notCreatedYet = true;
+
+function Start () {
+	initialRepeatAfter = repeatAfter;
+	initialPosition = transform.position;
+	notCreatedYet = true;
+}
+
+function Update () {
+	transform.position.x -= moveHowMuch;
+	repeatAfter -= moveHowMuch;
+	if ((repeatAfter < initialRepeatAfter / 2) && notCreatedYet) {
+		BroadcastMessage("RandomizeNext", SendMessageOptions.DontRequireReceiver);
+		notCreatedYet = false;
+	}
+	if (repeatAfter < 0) {
+		BroadcastMessage("DestroyLast", SendMessageOptions.DontRequireReceiver);
+		repeatAfter = initialRepeatAfter;
+		transform.position = initialPosition;
+		notCreatedYet = true;
+	}
+}
---
+++ Assets/scripts/RandomizeSprite.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+#pragma downcast
+
+var newObjPositionX = 0.0;
+var objects : GameObject[];
+private var lastObject : GameObject;
+private var evenOdd = true;
+
+function Start () {
+	evenOdd = true;
+	RandomizeNext();
+}
+
+function DestroyLast () {
+	Destroy(lastObject);
+}
+
+function RandomizeNext () {
+	var rand = Mathf.Abs(Random.value * objects.Length);
+	lastObject = Instantiate(objects[rand]);
+	lastObject.transform.parent = transform;
+	
+	evenOdd = !evenOdd;
+	var X = 0;
+	if (evenOdd) X = newObjPositionX;
+	lastObject.transform.localPosition = new Vector3(X, 0, 0);
+}
